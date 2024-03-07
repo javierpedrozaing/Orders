4c0ac3c4ae56db809ad84726bfc22302dd2bc245
Javier Pedroza <jpedroza@spreedly.com>
2024-03-06 22:31:14 -0500

    page component FrontEnd

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -7,8 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7171///") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5262/") });
 
+// configuramos la inyección del repositorio
 builder.Services.AddScoped<IRepository, Repository>(); // Notacion diamante porque tambien es un genérico
 
 await builder.Build().RunAsync();
