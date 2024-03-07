e76c099fbecd81203def186b04d2b635eb9bb0d8
Javier Pedroza <jpedroza@spreedly.com>
2024-03-06 21:30:17 -0500

    begin repository pattern

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7171///") });
+
+builder.Services.AddScoped<IRepository, Repository>(); // Notacion diamante porque tambien es un genérico
 
 await builder.Build().RunAsync();
 
---
+++ Orders/Orders.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Orders.Frontend.Repositories
+{
+	public interface IRepository
+	{
+		Task<HttpResponseWrapper<T>> GetAsync<T>(String url);
+
+		Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model); // post que no devuelve respuesta
+
+		Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model); // post que devuelve respuesta
+    }
+}
+
