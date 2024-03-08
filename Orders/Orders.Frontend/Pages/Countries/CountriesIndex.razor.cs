fbd9302e6db3970a4e516cf4e50bea15760cd4bf
Javier Pedroza <jpedroza@spreedly.com>
2024-03-07 21:02:40 -0500

    create and use generic component

---
+++ Orders/Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+	public partial class CountriesIndex // partial significa que hay dos clases que se llaman lo mismo, pero copiladas generan una sola
+	{
+        [Inject] private IRepository repository { get; set; } = null;
+
+        public List<Country>? Countries { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync(); // inicialización por defecto
+            var responseHttp = await repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
+
---
+++ Orders/Orders.Frontend/Shared/GenericList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Orders.Frontend.Shared
+{
+	public partial class GenericList<Titem>
+	{
+		[Parameter] public RenderFragment? Loading { get; set; }
+
+        [Parameter] public RenderFragment? NoRecords { get; set; }
+
+        [EditorRequired, Parameter] public RenderFragment? Body { get; set; } // EditorRequired para especificar parametros obligatorios
+
+        [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null;
+    }
+}
+
